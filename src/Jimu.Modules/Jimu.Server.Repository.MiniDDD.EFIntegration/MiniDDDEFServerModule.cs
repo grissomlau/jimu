866a671d363ea3d55862dec350c779ad32cb17b1
Grissom <grissomlau@qq.com>
2019-03-28 17:15:58 +0800

    integrate MiniDDD Repository module

---
+++ src/Jimu.Modules/Jimu.Server.Repository.MiniDDD.EFIntegration/MiniDDDEFServerModule.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Autofac;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using MiniDDD.UnitOfWork;
+using MiniDDD.UnitOfWork.EF;
+
+namespace Jimu.Server.Repository.MiniDDD.EFIntegration
+{
+    public class MiniDDDEFServerModule : ServerModuleBase
+    {
+        readonly MiniDDDEFOptions _options;
+        IContainer _container = null;
+        public MiniDDDEFServerModule(IConfigurationRoot jimuAppSettings) : base(jimuAppSettings)
+        {
+            _options = jimuAppSettings.GetSection(typeof(MiniDDDEFOptions).Name).Get<MiniDDDEFOptions>();
+        }
+
+        public override void DoServiceRegister(ContainerBuilder serviceContainerBuilder)
+        {
+            if (_options != null)
+            {
+                DbContextOptions dbContextOptions = new DbContextOptions
+                {
+                    ConnectionString = _options.ConnectionString,
+                    DbType = _options.DbType
+                };
+                Action<LogLevel, string> logAction = null;
+                if (_options.OpenLogTrace)
+                {
+                    logAction = (level, log) =>
+                    {
+                        if (_container != null && _container.IsRegistered<Jimu.Logger.ILogger>())
+                        {
+                            Jimu.Logger.ILogger logger = _container.Resolve<Jimu.Logger.ILogger>();
+                            logger.Info($"EF - LogLevel: {level} - {log}");
+                        }
+                    };
+                }
+
+                DefaultDbContext dbContext = new DefaultDbContext(dbContextOptions, _options.TableModelAssemblyName, logAction, _options.LogLevel);
+                serviceContainerBuilder.RegisterType<UnitOfWork>()
+                    .WithMetadata("context", dbContext)
+                    .InstancePerLifetimeScope();
+            }
+            base.DoServiceRegister(serviceContainerBuilder);
+        }
+
+        public override void DoInit(IContainer container)
+        {
+            _container = container;
+            base.DoServiceInit(container);
+        }
+    }
+}
---
+++ src/Jimu.Modules/Jimu.Server.Repository.MiniDDD.SqlSugarIntegration/MiniDDDSqlSugarServerModule.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+using Microsoft.Extensions.Configuration;
+using MiniDDD.UnitOfWork;
+using MiniDDD.UnitOfWork.SqlSugar;
+
+namespace Jimu.Server.Repository.MiniDDD.SqlSugarIntegration
+{
+    public class MiniDDDSqlSugarServerModule : ServerModuleBase
+    {
+        readonly MiniDDDSqlSugarOptions _options;
+        IContainer _container = null;
+        public MiniDDDSqlSugarServerModule(IConfigurationRoot jimuAppSettings) : base(jimuAppSettings)
+        {
+            _options = jimuAppSettings.GetSection(typeof(MiniDDDSqlSugarOptions).Name).Get<MiniDDDSqlSugarOptions>();
+        }
+
+
+        public override void DoServiceRegister(ContainerBuilder serviceContainerBuilder)
+        {
+            if (_options != null)
+            {
+                DbContextOptions dbContextOptions = new DbContextOptions
+                {
+                    ConnectionString = _options.ConnectionString,
+                    DbType = _options.DbType
+                };
+                Action<string> logAction = null;
+                if (_options.OpenLogTrace)
+                {
+                    logAction = (log) =>
+                    {
+                        if (_container != null && _container.IsRegistered<Jimu.Logger.ILogger>())
+                        {
+                            Jimu.Logger.ILogger logger = _container.Resolve<Jimu.Logger.ILogger>();
+                            logger.Info($"SqlSugar - {log}");
+                        }
+                    };
+                }
+
+                serviceContainerBuilder.RegisterType<UnitOfWork>()
+                    .WithParameter("options", dbContextOptions)
+                    .WithParameter("logAction", logAction).InstancePerLifetimeScope();
+            }
+            base.DoServiceRegister(serviceContainerBuilder);
+        }
+
+        public override void DoInit(IContainer container)
+        {
+            _container = container;
+            base.DoServiceInit(container);
+        }
+    }
+}
