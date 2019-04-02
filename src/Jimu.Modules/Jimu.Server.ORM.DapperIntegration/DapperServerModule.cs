6b04933e2a75874628a35f111515c3601578b135
Grissom <grissomlau@qq.com>
2019-04-02 10:28:21 +0800

    integrate dapper

---
+++ src/Jimu.Modules/Jimu.Server.ORM.DapperIntegration/DapperServerModule.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Data.SQLite;
+using System.Text;
+using Autofac;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using Npgsql;
+using Oracle.ManagedDataAccess.Client;
+
+namespace Jimu.Server.ORM.DapperIntegration
+{
+    public class DapperServerModule : ServerModuleBase
+    {
+        readonly DapperOptions _options;
+        public DapperServerModule(IConfigurationRoot jimuAppSettings) : base(jimuAppSettings)
+        {
+            _options = JimuAppSettings.GetSection(typeof(DapperOptions).Name).Get<DapperOptions>();
+        }
+
+        public override void DoServiceRegister(ContainerBuilder serviceContainerBuilder)
+        {
+            if (_options != null)
+            {
+                serviceContainerBuilder.Register((context) =>
+                {
+                    DbConnection cnn = null;
+                    switch (_options.DbType)
+                    {
+                        case DbType.MySQL:
+                            cnn = new MySqlConnection(_options.ConnectionString);
+                            break;
+                        case DbType.SQLServer:
+                            cnn = new SqlConnection(_options.ConnectionString);
+                            break;
+                        case DbType.Oracle:
+                            cnn = new OracleConnection(_options.ConnectionString);
+                            break;
+                        case DbType.PostgreSQL:
+                            cnn = new NpgsqlConnection(_options.ConnectionString);
+                            break;
+                        case DbType.SQLite:
+                            cnn = new SQLiteConnection(_options.ConnectionString);
+                            break;
+                        default:
+                            throw new Exception("please specify DbType!");
+                    }
+                    return cnn;
+                }).As<DbConnection>().InstancePerLifetimeScope();
+
+            }
+            base.DoServiceRegister(serviceContainerBuilder);
+        }
+    }
+}
---
+++ src/Jimu.Modules/Jimu.Server.Repository.MiniDDD.DapperIntegration/MiniDDDDapperServerModule.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+using Microsoft.Extensions.Configuration;
+using MiniDDD.UnitOfWork;
+
+namespace Jimu.Server.Repository.MiniDDD.DapperIntegration
+{
+    public class MiniDDDDapperServerModule : ServerModuleBase
+    {
+        readonly MiniDDDDapperOptions _options;
+        public MiniDDDDapperServerModule(IConfigurationRoot jimuAppSettings) : base(jimuAppSettings)
+        {
+            _options = jimuAppSettings.GetSection(typeof(MiniDDDDapperOptions).Name).Get<MiniDDDDapperOptions>();
+        }
+
+        public override void DoServiceRegister(ContainerBuilder serviceContainerBuilder)
+        {
+            if (_options != null)
+            {
+                DbContextOptions dbContextOptions = new DbContextOptions
+                {
+                    ConnectionString = _options.ConnectionString,
+                    DbType = _options.DbType
+                };
+
+                serviceContainerBuilder.RegisterType<IUnitOfWork>().WithParameter("options", dbContextOptions).InstancePerLifetimeScope();
+
+            }
+            base.DoServiceRegister(serviceContainerBuilder);
+        }
+
+    }
+}
